4f353d94d19d5cb59285b3d285912bc851a1c5a0
dhepeyuvi <dhepe@perfacct.eu>
2024-05-16 18:41:28 +0200

    Adding nvim config

---
+++ nvim_config/nvim/lua/ysd/plugins/init.lua
@@ -0,0 +1,7 @@
+return {
+"nvim-lua/plenary.nvim", -- lua functions that many plugins use 
+"christoomey/vim-tmux-navigator", -- tmux & split window navigation 
+"m4xshen/autoclose.nvim" -- auto close the brackets 
+}
+
+
