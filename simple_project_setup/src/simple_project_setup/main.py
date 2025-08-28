3259d5e989b963d4baa6530b5332d7c496744164
Yuvraj-Dhepe <yuvi.kiit@gmail.com>
2025-08-28 20:20:48 +0200

    WIP

---
+++ simple_project_setup/src/simple_project_setup/main.py
@@ -0,0 +1,17 @@
+import os
+
+
+def main():
+    """
+    Main function to demonstrate the project setup
+    Main function to demonstrate the project setup
+    Main function to demonstrate the project setup
+    Main function to demonstrate the project setup
+    Main function to demonstrate the project setup
+    """
+    print("Hello from simple-project-setup!")
+    print(os.getcwd())
+
+
+if __name__ == "__main__":
+    main()
